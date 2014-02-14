9b8e7304abb0303e3f1751ec0c693b49f2a8d3b3
Hugo <hebfirth@gmail.com>
2014-02-14 13:02:50 +0000

    Some bug fixes to hopefully make the project build more easily in Windows environments.

--- src/test/java/uk/ac/ncl/csc8101/BasicCypherInteractionTest.java
+++ src/test/java/uk/ac/ncl/csc8101/BasicCypherInteractionTest.java
@@ -12,7 +12,9 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
@@ -60,7 +62,8 @@ public class BasicCypherInteractionTest {
      */
     private static String readFile(String path, Charset encoding) throws IOException
     {
-        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        Path filePath = FileSystems.getDefault().getPath(path);
+        byte[] encoded = Files.readAllBytes(filePath);
         return encoding.decode(ByteBuffer.wrap(encoded)).toString();
     }
 
